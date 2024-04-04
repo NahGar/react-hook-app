8e941db0b85225e946ced84803cfc2fcc8323e3e
Nahuel Garcia <nhlgrc@hotmail.com>
2024-04-04 11:15:13 -0300

    adsa

--- src/03-examples/MultipleCustomHook.jsx
+++ src/03-examples/MultipleCustomHook.jsx
@@ -6,18 +6,22 @@ import { PokemonCard } from './PokemonCard';
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 export const MultipleCustomHook = () => {
-
+yar
     const { counter, increment, decrement } = useCounter( 1 );
 
     const { data, isLoading, hasError } = useFetch( url + counter );
-    
+
+    console.log(data.id);
+    console.log(data.name);
+    console.log(data.sprites);
+
     return (
         <>
-            <h1>Información de Pokemon</h1>
+            <h1>Información1 de Pokemon</h1>
             <hr />
 
             { /* !isLoading &&  <LoadingMessage /> */ }
-            { isLoading ? <LoadingMessage /> : <PokemonCard info={data} /> } 
+            { isLoading ? <LoadingMessage /> : <PokemonCard id={data.id} name={data.name} sprites={data.sprites} /> } 
 
             { /*data !== null && <h2>{ data.name }</h2>*/ }
             { /*data !== null && <pre>{ JSON.stringify( data, null, 2)}</pre>*/ }
--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -1,10 +1,10 @@
 
-import React from 'react'
+import React from 'react';
 
+/*
 //export const PokemonCard = ( { id, name, sprites = [] } ) => {
-
 export const PokemonCard = ( {info} ) => {
-    console.log(info);
+    
     return (
         <section style={{ height: 200 }}>
           <h2 className='text-capitalize'>#{info?.id} - {info?.name}</h2>
@@ -14,6 +14,24 @@ export const PokemonCard = ( {info} ) => {
             <img src={ info.sprites.back_shiny } style={{ height: 200 }} />
             <img src={ info.sprites.front_default } style={{ height: 200 }} />
             <img src={ info.sprites.front_shiny } style={{ height: 200 }} />
+            
+          </div>
+        </section>
+    )
+}
+*/
+
+export const PokemonCard = ( {id, name, sprites = [] } ) => {
+    
+    return (
+        <section style={{ height: 300 }} >
+          <h2 className='text-capitalize'>#{id} - {name}</h2>
+        
+          <div>
+            <img src={ sprites.back_default } style={{ height: 200 }} />
+            <img src={ sprites.back_shiny } style={{ height: 200 }} />
+            <img src={ sprites.front_default } style={{ height: 200 }} />
+            <img src={ sprites.front_shiny } style={{ height: 200 }} />
             
           </div>
         </section>
