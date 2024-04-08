bdc9c5170f79c897a84568b3bfa10816b66a8e9e
Nahuel Garcia <nhlgrc@hotmail.com>
2024-04-08 11:59:36 -0300

    sss

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,11 @@ import './index.css'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 //import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+//import { Layout } from './05-useLayoutEffect/Layout'
+import { Padre } from './07-tarea-memo/Padre'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <Padre />
   </React.StrictMode>,
 )
